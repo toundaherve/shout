b05e80186bb5574d180940af511ff525b3f2bbcd
toundaherve <toundaherve@gmail.com>
2020-12-30 16:47:01 +0000

    Add Search layout

--- src/components/Hear.tsx
+++ src/components/Hear.tsx
@@ -23,28 +23,28 @@ const Hear = (props: RouteComponentProps) => {
   }
 
   return (
-    <div className="row">
+    <div className="row py-5 gy-4 gy-md-5">
       <div className="col-12">
-        <h1 className="text-center">Find what people want</h1>
-        <form
-          className="d-flex justify-content-center"
-          onSubmit={handleHearVoice}
-        >
+        <h1 className="text-center text-light mb-3 mb-md-4">
+          Find what people want{" "}
+        </h1>
+        <form className="d-flex" onSubmit={handleHearVoice}>
           <input
             onChange={handleChangeVoiceMessage}
             value={voiceMessage}
-            type="text"
-            placeholder="Type voice"
-            className="hear-input"
+            className="w-100 me-2 d-block form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
           />
-          <button className="hear-submission" type="submit">
+          <button className="btn btn-outline-success" type="submit">
             Hear now !
           </button>
         </form>
       </div>
 
-      <div className="col-12">
-        <h2 className="text-left">Results</h2>
+      <div classNamejustify-content-center="col-12">
+        <h2 className="text-left text-light mb-3">Results</h2>
         <VoiceList voiceListData={heardVoices} />
       </div>
     </div>
