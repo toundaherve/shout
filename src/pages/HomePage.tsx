8b2343016859fa9f8252e9b1699a03774fe667ea
toundaherve <toundaherve@gmail.com>
2021-01-01 22:47:12 +0000

    Refactor HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,27 +2,72 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 
-const Breadcrumb = () => {
+const HomePage = () => {
   return (
-    <nav aria-label="breadcrumb d-block">
-      <ol className="breadcrumb">
-        <li className="breadcrumb-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="breadcrumb-item active" aria-current="page">
-          Library
-        </li>
-      </ol>
-    </nav>
+    <Layout>
+      <UnifiedHeader />
+      <span className="d-block mb-2"></span>
+    </Layout>
   );
 };
+
+function UnifiedHeader() {
+  const mockFilters = ["category", "condition"];
+  return (
+    <div className="UnifiedHeader">
+      <div className="flex-wrap">
+        <span className="d-block mb-2 flex-grow-1 flex-shrink-1 w-100"></span>
+        <nav aria-label="breadcrumb d-block">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Library
+            </li>
+          </ol>
+        </nav>
+        <h1>Ads for "Iphone" for Sale</h1>
+        <div className="d-block flex-nowrap">
+          <div className="d-none d-lg-block">
+            <div className="d-flex flex-wrap">
+              <span className="flex-grow-1 bg-dark"></span>
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Basic outlined example"
+              >
+                <button type="button" className="btn btn-outline-primary">
+                  {"<-"}
+                </button>
+                <button type="button" className="btn btn-outline-primary">
+                  {"->"}
+                </button>
+              </div>
+            </div>
+            <span className="d-block mb-3"></span>
+          </div>
+          {/* TODO: Overflow Not Working - Check with lots of filters - Fix overflow-x: auto|hidden*/}
+          <div className="d-flex flex-nowrap overflow-auto">
+            {mockFilters.map((filter, idx) => (
+              <div className="d-flex px-1 py-2" key={idx}>
+                <Filter name={filter} options={["Electronics", "Fashion"]} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <span className="d-block mb-3"></span>
+      </div>
+    </div>
+  );
+}
 
 interface FilterProps {
   name: string;
   options: Array<string>;
 }
 
-const Filter = (props: FilterProps) => {
+function Filter(props: FilterProps) {
   return (
     <div className="dropdown">
       <button
@@ -43,66 +88,6 @@ const Filter = (props: FilterProps) => {
       </ul>
     </div>
   );
-};
-
-const Filters = () => {
-  const mockFilters = [
-    "category",
-    "condition",
-    "brand",
-    "price",
-    "date listed",
-    "location",
-    "delivery options",
-    "sort",
-  ];
-  return (
-    <div className="d-block flex-nowrap">
-      <div className="d-none d-lg-block">
-        <div className="d-flex flex-wrap">
-          <span className="flex-grow-1 bg-dark"></span>
-          <div
-            className="btn-group"
-            role="group"
-            aria-label="Basic outlined example"
-          >
-            <button type="button" className="btn btn-outline-primary">
-              {"<-"}
-            </button>
-            <button type="button" className="btn btn-outline-primary">
-              {"->"}
-            </button>
-          </div>
-        </div>
-        <span className="d-block mb-3"></span>
-      </div>
-      {/* TODO: Overflow Not Working - Fix overflow-x: auto|hidden*/}
-      <div className="d-flex flex-nowrap overflow-auto">
-        {mockFilters.map((filter, idx) => (
-          <div className="d-flex px-1 py-2" key={idx}>
-            <Filter name={filter} options={["Electronics", "Fashion"]} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const HomePage = () => {
-  return (
-    <Layout>
-      <div className="UnifiedHeader">
-        <div className="flex-wrap">
-          <span className="d-block mb-2 flex-grow-1 flex-shrink-1 w-100"></span>
-          <Breadcrumb />
-          <h1>Ads for "Iphone" for Sale</h1>
-          <Filters />
-          <span className="d-block mb-3"></span>
-        </div>
-      </div>
-      <span className="d-block mb-2"></span>
-    </Layout>
-  );
-};
+}
 
 export default HomePage;
